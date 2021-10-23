3b5a8e8142bff44754c7d08b848a03e97a099f52
Bhumi Parekh <bhumiparekh02@gmail.com>
2021-10-23 16:18:58 +0530

    TRELLO: 3 COMMENT: ADD CONTROLLERS IN BLAZOR SERVER APP

---
+++ CMS.UI/Server/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using CMS.API.Utilities;
+using CMS.Data.ContextModels;
+using CMS.Data.FormModels;
+using CMS.Services.Interface;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.UI.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly JWTTokenGenerator _jWTTokenGenerator;
+        private readonly IUserService _userService;
+        private readonly IConfiguration _config;
+        private ApplicationContext _context;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, ILogger<AccountController> logger, IUserService userService, ApplicationContext context, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _logger = logger;
+            _jWTTokenGenerator = new JWTTokenGenerator(config);
+            _userService = userService;
+            _context = context;
+            _config = config;
+        }
+
+        public async Task<IActionResult> Register(RegisterFM registerModel)
+        {
+
+        }
+
+        public async Task<IActionResult> Login(LoginFM loginUser)
+        {
+        }
+
+    }
+}
---
+++ CMS.UI/Server/Controllers/SuperAdminController.cs
@@ -0,0 +1,56 @@
+﻿using CMS.Data.ContextModels;
+using CMS.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.UI.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SuperAdminController : Controller
+    {
+        private readonly ILogger<SuperAdminController> _logger;
+        private readonly IUserService _userService;
+        private readonly ILawyerService _lawyerService;
+        private IConfiguration _config;
+        private ApplicationContext _context;
+        public SuperAdminController(ILogger<SuperAdminController> logger, ILawyerService lawyerService, IUserService userService, ApplicationContext context, IConfiguration config)
+        {
+            _logger = logger;
+            _userService = userService;
+            _lawyerService = lawyerService;
+            _context = context;
+            _config = config;
+        }
+
+        public void AddAdmin()
+        {
+
+        }
+
+        public void AddLawyer()
+        {
+
+        }
+
+        public void UpdateAdmin()
+        {
+
+        }
+
+        public void LawyerDetails()
+        {
+
+        }
+
+        public void RemoveLawyer(int lawyerId)
+        {
+
+        }
+    }
+}
